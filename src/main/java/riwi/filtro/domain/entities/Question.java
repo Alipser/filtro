81c8c1301f00bb3ef016a993131d7c5a144cfe88
Alipser <Romarioajb@outlook.com>
2024-06-04 07:10:01 -0500

    Question Entity Finished

--- src/main/java/riwi/filtro/domain/entities/Question.java
+++ src/main/java/riwi/filtro/domain/entities/Question.java
@@ -1,5 +1,4 @@
 package riwi.filtro.domain.entities;
-
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
@@ -14,33 +13,27 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 // import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import riwi.filtro.utils.enums.QusetionType;
 // import lombok.ToString;
 import riwi.filtro.utils.enums.StatusEnum;
 
-@Entity(name = "surveys")
+@Entity(name = "questions")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Survey {
+public class Question {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(length = 255, nullable = false)
-    private String title;
-    @Column(nullable = true, columnDefinition = "TEXT")
-    private String description;
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String text;
     @Column(nullable = false)
-    private LocalDateTime creationDate;
+    private QusetionType type;
+    @Column(nullable = false)
     private StatusEnum status;
 
     @ManyToOne
-    @JoinColumn(name = "creator_id", referencedColumnName = "id")
-    private User creator;
-
-    // @ToString.Exclude
-    // @EqualsAndHashCode.Exclude
-    // @OneToMany(mappedBy = "survey")
-    // private List<Question> questions;
-
+    @JoinColumn(name = "survey_id", referencedColumnName = "id")
+    private Survey survey;
 }
